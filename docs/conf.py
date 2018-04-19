4bf837c41ca0e9a2d75757838249f18019330966
Jennifer Appleby <33709515+jenapp@users.noreply.github.com>
2018-04-19 16:45:38 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,18 +1,18 @@
-#html_theme_options = {
-#    'canonical_url': '',
-#    'analytics_id': '',
-#    'logo_only': False,
-#    'display_version': False,
-#    'prev_next_buttons_location': 'top',
-#    'style_external_links': False,
-#    'vcs_pageview_mode': '',
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',
+    'logo_only': False,
+    'display_version': False,
+    'prev_next_buttons_location': 'top',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
     # Toc options
-#    'collapse_navigation': True,
-#    'sticky_navigation': False,
-#    'navigation_depth': -1,
-#    'includehidden': True,
-#    'titles_only': False
-#}
+    'collapse_navigation': True,
+    'sticky_navigation': False,
+    'navigation_depth': -1,
+    'includehidden': True,
+    'titles_only': False
+}
 
 # The master toctree document.
 master_doc = 'index'
