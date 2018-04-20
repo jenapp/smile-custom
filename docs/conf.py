a0d3f29bcaeb7fc8a5933988db3382ac21369877
Jennifer Appleby <33709515+jenapp@users.noreply.github.com>
2018-04-20 14:39:12 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ html_theme_options = {
     'canonical_url': '',
     'analytics_id': '',
     'logo_only': False,
-    'display_version': True,
+    'display_version': False,
     'prev_next_buttons_location': 'top',
     'style_external_links': False,
     'vcs_pageview_mode': '',
