0f81093ef87f21afee82803ec5584243338b9a1b
Jennifer Appleby <33709515+jenapp@users.noreply.github.com>
2018-04-20 16:43:41 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,11 @@
 # The master toctree document.
 master_doc = 'index'
 
+project = 'Smile'
+
+html_logo = 'inomial-logo-rgb-72dpi-150px_.png'
+# html_static_path = ['_static']
+
 html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
 html_theme_options = {
