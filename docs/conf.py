1fc2880da3c21428306a13ec0bd5a4f1b9320d81
Jennifer Appleby <33709515+jenapp@users.noreply.github.com>
2018-04-20 14:38:12 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,3 +4,18 @@ master_doc = 'index'
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'top',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
