85b715b58014cc28215f7c4991d49c35d79df845
Jennifer Appleby <33709515+jenapp@users.noreply.github.com>
2018-04-20 14:19:53 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,5 @@ master_doc = 'index'
 html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
 html_theme_options = {
-    "rightsidebar": "true",
-    "relbarbgcolor": "black"
+    "rightsidebar": "true"
 }
